ec30a790150587f8dae97a2b3563962a6225f2d8
Jordan Blum <jordan@Jordans-MacBook-Pro-2.local>
2019-09-02 17:27:43 -0700

    fixed issues and added some good stuff. Make sure to verify the url, whitelist correct IP, etc. for mongodb

---
+++ connect.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+class Connect(object):
+    @staticmethod    
+    def get_connection():
+        user = os.environ['MDB_USERNAME']
+        password = os.environ['MDB_PASSWORD']
+        mongoLink = os.environ['MDB_LINK']
+        return MongoClient("mongodb://"+user+":"+password+"@"+MDB_LINK)
