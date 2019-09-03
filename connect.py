3fd3f806ec395703d4baede653a22da4ed52063b
Jordan Blum <jordan@Jordans-MacBook-Pro-2.local>
2019-09-02 23:55:49 -0700

    first heroku deployment

--- connect.py
+++ connect.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 class Connect(object):
     @staticmethod    
     def get_connection():
-        user = os.environ['MDB_USERNAME']
-        password = os.environ['MDB_PASSWORD']
-        mongoLink = os.environ['MDB_LINK']
-        return MongoClient("mongodb://"+user+":"+password+"@"+MDB_LINK)
+        #user = os.environ['MDB_USERNAME']
+        #password = os.environ['MDB_PASSWORD']
+        #mongoLink = os.environ['MDB_LINK']
+        return MongoClient("mongodb://admin:Homeslicer1@cluster0-shard-00-00-ftwiq.mongodb.net:27017,cluster0-shard-00-01-ftwiq.mongodb.net:27017,cluster0-shard-00-02-ftwiq.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
--- jordansJams.py
+++ jordansJams.py
@@ -1,65 +1,126 @@
 import os
-from flask import Flask, session
+import time
+from flask import Flask
 from flask_pymongo import PyMongo
 from pymongo import MongoClient
 from connect import Connect
-from twilio.rest import Client
+from twilio.twiml.messaging_response import Message, MessagingResponse
 from twilio.base import exceptions
+from twilio.rest import Client
 
 #add env vars to Heroku
 def twilioConnect():
-    twilioSID = os.environ['TWILIO_SID']
-    twilioAuthToken = os.environ['TWILIO_AUTH']
-    return Client(twilioSID, twilioAuthToken)
+    #twilioSID = os.environ['TWILIO_SID']
+    #twilioAuthToken = os.environ['TWILIO_AUTH']
+    return Client('AC0ed1eb8b97d3465050670f36b2f3e03f', '343e9844b3ad99c3eb22fca94bf57ba7')
 
-#workOn
 def is_subscriber(phoneNumber):
-    db = mongoConnect()
-    number = db.find({"Numbers": phoneNumber})
-    if(number.count() != 0):
+    db = Connect.get_connection().numbers
+    if db.find_one({"numbers": phoneNumber}) != None:
         return True
     return False
 
-#Work on
 def newUser():
-    db = mongoConnect()
-    numberCollection = db['jordans-jams']['Numbers']
-    numberCollection.insert({'phoneNumber':phoneNumber})
+    db = Connect.get_connection()
+    numbers = db.jordansJams.numbers
+    numbers.insert_one({'phoneNumber':phoneNumber})
     return getJams(newUser = true)
 
-#work On
-def addSongs():
-    verified = False
-    number = request.form['From']
+def removeUser(phoneNumber):
+    db = Connect.get_connection().jordansJams.numbers
+    db.delete_one({'phoneNumber': phoneNumber})
+
+def getJams(phoneNumber):
+
+    db = Connect.get_connection().jordansJams
+    twilioClient = twilioConnect()
+
+    songs = db.songs.find()
+    message = "Hello! Here are the newest jams for this week: "
+
+    twilioClient.messages.create(
+            to=phoneNumber,
+            from_="+14152124859",
+            body=message)
+
+    time.sleep(1)
+
+    for song in songs:
+        link = '{0}'.format(song['songLink'])
+        twilioClient.messages.create(
+            to=phoneNumber,
+            from_="+14152124859",
+            body=link)
+        time.sleep(.5)
+
+    return "Songs successfully sent."
+
+def verify(requestNumber):
+    #verify if this is an Admin phone number
+    #mask this later
+    if requestNumber == '+18478484251':
+        return True
+    else:
+        message_body = "Sorry, you are not authorized to add songs."
+        twilioClient.messages.create(to=requestNumber, from_="+14152124859", body=message_body)
+        return False
+
+def clearSongs(requestNumber):
+    db = Connect.get_connection()
+    numbers = db.jordansJams.numbers
+    songs = db.jordansJams.songs
+
+    #Create response to text back w/ twiml
+    resp = MessagingResponse()
+
+    #Clear all songs in database if more than 2 songs & message is 'clear'
+    if songs.count >= 2 and verify(requestNumber):
+        songs.remove({ })
+        message_body = "All songs have been cleared."
+        resp.message(message_body)
+        return str(resp)
+    
+
+def addSongs(requestNumber, requestMessage, sessionCount):
+
+    number = requestNumber
 
     db = Connect.get_connection()
     numbers = db.jordansJams.numbers
     songs = db.jordansJams.songs
 
-    counter = session.get('counter', 0)
-    counter += 1
-    session['counter'] = counter
-
     #Create response to text back w/ twiml
     resp = MessagingResponse()
+
+    #check if song already exists in database
+    if songs.find_one({"songLink":requestMessage}) != None:
+        message_body = "That song has already been added. Please add another."
+        resp.message(message_body)
+        return str(resp)
+        
+    #add song to database
+    if "open.spotify.com" in requestMessage and sessionCount >= 1:
+        songs.insert_one({"songLink":requestMessage})
+        print "Added song"
     
-    if number == '+18478484251':
-        verified = True
-    
-    if counter >= 1 and verified:
+    if sessionCount >= 1 and verify(number):
         if songs.count() == 0:
             message_body = "Please send in your 2 songs for the week."
             resp.message(message_body)
             return str(resp)
-        elif songs.count() == 1:
+        elif songs.count() == 1 and verify(number):
             message_body = "Please send in your 2nd song."
             resp.message(message_body)
             return str(resp)
-        elif songs.count == 2:
+        elif songs.count() == 2 and verify(number):
             message_body = "Thanks for sending in your weekly songs!"
             resp.message(message_body)
+            print sessionCount
             return str(resp)
         else:
-            message_body = "You already have 2 songs. Would you like to clear? If so, type CLEAR"
+            message_body = "There are 2 songs currently in the database. Would you like to clear? If so, type CLEAR"
             resp.message(message_body)
             return str(resp)
+
+    resp.message("Error adding weekly songs. Please check for maintenance.")
+    return str(resp)
