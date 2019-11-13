484dc3ffd257a612cbf7b1205ba0b4474cb7c30d
Jordan Blum <jordan@Jordans-MacBook-Pro-2.local>
2019-11-12 22:02:34 -0800

    adding more changes

--- clock.py
+++ clock.py
@@ -1,12 +1,7 @@
 from jordansJams import getJams, notifyJordan
-from pymongo import MongoClient
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler(timezone='US/Pacific')
-
-mongo = MongoClient()
-numbers = mongo.jordansJams.numbers
-
 
 @sched.scheduled_job('cron', day_of_week='tue', hour=21, minute=59, timezone="US/Pacific")
 def notifyJ():
