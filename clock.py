fffbde916dadc4c22c04cf04ebc3c894fcc9b3a4
Jordan Blum <jordan@Jordans-MacBook-Pro-2.local>
2019-11-12 22:22:17 -0800

    final changes on html & clock testing

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 
 sched = BlockingScheduler(timezone='US/Pacific')
 
-@sched.scheduled_job('cron', day_of_week='tue', hour=22, minute=5, timezone="US/Pacific")
+@sched.scheduled_job('cron', day_of_week='tue', hour=22, minute=30, timezone="US/Pacific")
 def notifyJ():
     print("it fired off the notification")
     notifyJordan()
