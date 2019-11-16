dca594be0661eedab7cadf875c8c5b618cf961bf
Jordan Blum <jordan@Jordans-MacBook-Pro-2.local>
2019-11-15 18:08:09 -0800

    final clock test

--- clock.py
+++ clock.py
@@ -1,11 +1,14 @@
 from jordansJams import getJams, notifyJordan
 from apscheduler.schedulers.blocking import BlockingScheduler
 
+
+def notifyJ():
+    print("Jordan has been notified")
+    notifyJordan()
+
+
 sched = BlockingScheduler(timezone='US/Pacific')
 
-@sched.scheduled_job('cron', day_of_week='fri', hour=17, minute=55, timezone="US/Pacific")
-def notifyJ():
-    print("it fired off the notification")
-    notifyJordan()
+sched.add_job(notifyJ, 'cron', day_of_week='fri', hour=18, minute=10, timezone="US/Pacific")
 
 sched.start()
