3ee64afe27ca1b9c5916f50b346c5374175a0127
Jordan Blum <jordan@Jordans-MacBook-Pro-2.local>
2019-11-17 15:38:12 -0800

    adding subscriber notification, changing getJams, and adding new frontend for website

--- clock.py
+++ clock.py
@@ -1,12 +1,22 @@
-from jordansJams import notifyJordan
+from jordansJams import notifyJordan, getJams
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+from connect import Connect
 
 def notifyJ():
     notifyJordan()
 
+def notifySubscribers():
+    numbers = Connect.get_connection().jordansJams.numbers
+    for number in numbers.find():
+        sendTo = number['_id']
+        getJams(sendTo)
+
+    
+
 sched = BlockingScheduler(timezone='US/Pacific')
 
-sched.add_job(notifyJ, 'cron', day_of_week='fri', hour=19, timezone="US/Pacific")
+sched.add_job(notifyJ, 'cron', day_of_week='tue', hour=18, timezone="US/Pacific")
+
+sched.add_job(notifySubscribers, 'cron', day_of_week='wed', hour=10, timezone="US/Pacific")
 
 sched.start()
