81a3dbc168bd36357cbc92458d0c56f9905cb1e3
Jordan Blum <jordan@Jordans-MacBook-Pro-2.local>
2019-11-17 23:33:10 -0800

    fixing ajax call

--- static/styles/script.js
+++ static/styles/script.js
@@ -4,29 +4,19 @@ if (window.matchMedia('(prefers-reduced-motion)').matches) {
   video.pause();
 }
 
-function postAjax(url, data, success) {
-    var params = typeof data == 'string' ? data : Object.keys(data).map(
-            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
-        ).join('&');
-
-    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-    xhr.open('POST', url);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
-    };
-    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.send(params);
-    return xhr;
-}
 
 function addNumber() {
+
+    link = "https://jordans-jams.herokuapp.com/addFromWeb/"
+
     var phoneNumber = document.getElementById('phone').value
     var fullNum = "+1" + phoneNumber
 
-    postAjax('https://jordans-jams.herokuapp.com/addFromWeb/', fullNum, function(data){ console.log(data); });
+    link += String(fullNum)
 
-    console.log("tried to add the number")
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", link, true);
+    xhttp.send();
 }
 
 
