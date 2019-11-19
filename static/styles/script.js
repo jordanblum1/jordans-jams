369c2a2278906c250378f870bda78b69272c019e
Jordan Blum <jordan@Jordans-MacBook-Pro-2.local>
2019-11-18 22:15:09 -0800

    fixing some vulnerabilites & adding some style changes to the UI

--- static/styles/script.js
+++ static/styles/script.js
@@ -4,22 +4,33 @@ if (window.matchMedia('(prefers-reduced-motion)').matches) {
   video.pause();
 }
 
+function isValid(number) {
+    var phoneRegEx = /^[2-9]\d{2}[2-9]\d{2}\d{4}$/;
+    var digits = number.replace(/\D/g, "");
+    return phoneRegEx.test(digits);
+  }
+
 
 function addNumber() {
 
-    link = "https://jordans-jams.herokuapp.com/addFromWeb/"
+    link = "https://jordans-jams.herokuapp.com/addFromWeb/";
+    var phoneNumber = document.getElementById('phone').value;
 
-    var phoneNumber = document.getElementById('phone').value
-    var fullNum = "+1" + phoneNumber
+    if(isValid(phoneNumber) && phoneNumber.length >= 10){
+        var fullNum = "+1" + phoneNumber;
 
-    link += String(fullNum)
+        link += String(fullNum);
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", link, true);
-    xhttp.send();
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("POST", link, true);
+        xhttp.send();
 
-    document.getElementById('phone').value = " "
-    alert("Thank's for signing up for Jordan's Jams. Hope you enjoy!")
+        document.getElementById('phone').value = "Success!";
+        alert("Thank's for signing up for Jordan's Jams. Hope you enjoy!");
+    }
+    else{
+        alert("The phone number you have entered is invalid. Please try again.");
+    }
 }
 
 
