010627aa176dbf7cd654b9b3c7afcfe47ef8c559
jordan.blum <jordan.blum@workday.com>
2020-04-04 23:44:13 -0700

    fixing spotipy stuff & updating in jordans jams

--- trackInfo.py
+++ trackInfo.py
@@ -8,15 +8,10 @@ token = util.oauth2.SpotifyClientCredentials(client_id=os.environ['SPOT_ID'], cl
 cache_token = token.get_access_token()
 spotify = spotipy.Spotify(cache_token)
 
-def getURI(spotifyURL):
-    return ("spotify:track:" + spotifyURL.replace('https://', '').split('/')[2].split('?')[0])
-
-def getTrackInfo(uri):
-    return spotify.track(uri)
-
-def parseTrack(uri, url):
+def getTrackInfo(url):
     spotifyLink = url
-    track = getTrackInfo(uri)
+    track = spotify.track(url)
     trackName =  track['name']
     artistName = track['artists'][0]['name']
+    uri = track['uri']
     return {"uri":uri, "track":trackName, "artist":artistName, "link":spotifyLink}
