c6dbd4541e5c7bbb8aec94877d449c125f4609e9
Jordan Blum <jordanblum16@gmail.com>
2024-04-30 20:25:54 -0400

    update with logging SMS

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from flask import Flask, request, session, render_template
 from connect import Connect
 from twilio.twiml.messaging_response import MessagingResponse
@@ -26,9 +27,7 @@ def web_add(number):
     else:
         return new_user(number)
 
-
 @app.route('/jams', methods=['POST'])
-
 def sms():
     '''used for all communications of text'''
     resp = MessagingResponse()
@@ -39,24 +38,30 @@ def sms():
     counter += 1
     session['counter'] = counter
 
-    print(inbound_message)
-    print(counter)
+    logging.info(f"Received message: {inbound_message}")
+    logging.info(f"Counter: {counter}")
 
     exit_words = ["STOP", "END", "UNSUBSCRIBE", "REMOVE"]
     if inbound_message.upper() in exit_words:
+        logging.info("Executing remove_user method")
         return remove_user(number)
 
     if inbound_message == "CLEAR":
         session['counter'] = 0
+        logging.info("Executing clear_songs method")
         return clear_songs(number)
     if inbound_message == "JAMS":
+        logging.info("Executing get_jams method")
         return get_jams(number)
     if (inbound_message == "ADD") or (counter >= 1 and "open.spotify.com" in inbound_message.lower()):
         if verify(number):
+            logging.info("Executing add_songs method")
             return add_songs(number, inbound_message, counter)
     if not is_subscriber(number):
+        logging.info("Executing new_user method")
         return new_user(number)
     if is_subscriber(number):
+        logging.info("Executing default_message method")
         return default_message(number)
 
     message_body = 'Error, that is not a possible entry.'
